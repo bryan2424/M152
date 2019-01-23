889612e4a183b5c08c3c90ff8e778873eb4f950c
Bryan Villafuerte <bryan.vllfr@eduge.ch>
2019-01-23 14:02:30 +0100

    creation de la page home

---
+++ view/home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="./bootstrap-4.2.1-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <title>Blog</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <ul class="navbar-nav">
+                <a class="navbar-brand" href="#">Logo</a>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="posts.php">Post</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+                <form class="form-inline" action="">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-info" type="submit">Search</button>
+                </form>
+            </ul>
+        </nav>
+        <div class="container">
+            <br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2>Bonjour</h2>
+                    <h5>Username</h5>
+                    <img>
+                    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                    <h3>infos</h3>
+                    <p>Lorem ipsum dolor sit ame.</p>
+                </div>  
+                <div class="col-sm-8">
+                    <h2>TITRE POST</h2>
+                    <h5>description, Dec 7, 2017</h5>
+                    <div class="fakeimg">Fake Image</div>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    <br>
+                    <h2>TITRE POST</h2>
+                    <h5>description, Dec 7, 2017</h5>
+                    <div class="fakeimg">Fake Image</div>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="./bootstrap-4.2.1-dist/js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
