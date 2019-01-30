5981fcc939d3cb0a79760370065692104fcab925
Bryan Villafuerte <bryan.vllfr@eduge.ch>
2019-01-30 14:52:37 +0100

    function getPosts

--- model/backend.php
+++ model/backend.php
@@ -1,5 +1,15 @@
 <?php
 require_once './model/BDD.php';
+
+function getPosts()
+{
+    $connexion = getConnexion();
+    $req = $connexion->prepare('SELECT * FROM posts');
+    $req->execute();
+    $result = $req->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
 function insertPost($commentaire, $typeMedia, $nomMedia, $datePost) {
        $connexion = getConnexion();
        $requete = $connexion->prepare("INSERT INTO posts (commentaire, typeMedia, nomMedia, datePost) VALUES (:commentaire, :typeMedia, :nomMedia, :datePost)");
