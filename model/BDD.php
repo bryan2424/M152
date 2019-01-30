4685655381025dcc92c148b037ed425f1046c3a9
Bryan Villafuerte <bryan.vllfr@eduge.ch>
2019-01-30 14:50:27 +0100

    correction de la connexion à la bdd

--- model/BDD.php
+++ model/BDD.php
@@ -1,15 +1,18 @@
 <?php
-function getConnexion() {
-        $server = '127.0.0.1';
-        $username = 'root';
-        $password = '';
-        $dbName = 'M152';
 
-        static $dbd = null;
-        try {
-            $dbd = new PDO("mysql:host=$server;dbname=$dbName", $username, $password);
-        } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-        return $dbd;
+DEFINE('SERVER',"127.0.0.1");
+DEFINE('USERNAME',"adminM152");
+DEFINE('PASSWORD',"adminM152");
+DEFINE('DBNAME',"M152");
+
+function getConnexion() {
+
+    static $dbd = null;
+    try {
+        $dbd = new PDO('mysql:host='.SERVER.';dbname='.DBNAME, USERNAME, PASSWORD);
+        $dbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+    return $dbd;
 }
